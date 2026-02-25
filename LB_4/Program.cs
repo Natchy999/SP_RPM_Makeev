b4c4888e9b25c6f100319a38996d79426ae2451a
Natchy999 <173190443+Natchy999@users.noreply.github.com>
2026-02-25 20:14:32 +0500

    Ye
    Today I saw a leaf that looked like a frog and I was like haha nice

---
+++ LB_4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace LB_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processes = Process.GetProcesses();
+
+            Console.WriteLine($"Все запущенные процессы на компьютере ({processes.Count()}):");
+            foreach (Process process in processes)
+            {
+                Console.WriteLine($"ID: {process.Id} | Имя: {process.ProcessName}");
+            }
+
+            Console.WriteLine("\nДля продолжения нажмите любую клавишу..."); Console.ReadKey();
+            
+            Process vsproc = Process.GetProcessesByName("devenv")[0];
+            ProcessThreadCollection vsprocThreads = vsproc.Threads;
+            Console.WriteLine($"\nВсе потоки процесса Visual Studio ({vsprocThreads.Count})");
+            foreach (ProcessThread procThread in vsprocThreads)
+            {
+                Console.WriteLine($"ThreadID: {procThread.Id}");
+            }
+
+            Console.WriteLine("\nДля продолжения нажмите любую клавишу..."); Console.ReadKey();
+
+            Console.WriteLine("\nЗапуск процесса mspaint.exe");
+            Process.Start("mspaint.exe");
+            Console.WriteLine("\nЗапуск процесса msedge.exe");
+            Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe");
+
+            Console.WriteLine("\nДля продолжения нажмите любую клавишу..."); Console.ReadKey();
+        }
+    }
+}
